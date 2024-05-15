f4706003d5bcfa6e77c17a679fe7af7756885886
vinicius_engelmann <viniengelmann@gmail.com>
2024-05-15 19:49:06 -0300

    Add method create

---
+++ src/Services/Create.java
@@ -0,0 +1,15 @@
+package Services;
+
+import java.util.Scanner;
+
+public class Create {
+    int arrayIds = Statics.GlobalArray.array[10];
+
+    Scanner tec = new Scanner(System.in);
+
+   public void create(){
+        System.out.println("Digite o nome do CRUD:");
+
+    }
+
+}
---
+++ src/Statics/GlobalArray.java
@@ -0,0 +1,6 @@
+package Statics;
+import java.util.Scanner;
+
+public class GlobalArray {
+    public static int[] array = new int[10];
+}
